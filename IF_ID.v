e6d8d493e0bd739e86e2e47341700183ccfd1e17
b03201003 <b03201003@ntu.edu.tw>
2018-12-02 00:23:07 +0800

    registers and pipelined_CPU, unfinished

---
+++ IF_ID.v
@@ -0,0 +1,35 @@
+module IF_ID (PC_plus4_in, PC_plus4_out, instruction_in, instruction_out, IF_ID_Write, IF_Flush, clk, reset);
+	// 1. data content
+	input [31:0] PC_plus4_in, instruction_in;
+	output [31:0] PC_plus4_out, instruction_out;
+	// 2. hazard control
+	// IF_ID_Write: sync; if (IF_ID_Write==1'b0), do not update content at this rising edge
+	// IF_Flush: sync; if (IF_Flush==1), clear ALL content, NOT ONLY control signals
+	input IF_ID_Write, IF_Flush;
+	// 3. general contorl
+	// reset: async; set all register content to 0
+	input clk, reset;
+
+	reg [31:0] PC_plus4_out, instruction_out;
+
+	always @(posedge clk or posedge reset)
+	begin
+		if (reset==1'b1)
+		begin
+			PC_plus4_out <= 32'b0;
+			instruction_out <= 32'b0;
+		end
+		else if (IF_Flush==1'b1)
+		begin
+			PC_plus4_out <= 32'b0;
+			instruction_out <= 32'b0;
+		end
+		else if (IF_ID_Write==1'b1)
+		begin
+			PC_plus4_out <= PC_plus4_in;
+			instruction_out <= instruction_in;
+		end
+
+	end
+	
+endmodule
