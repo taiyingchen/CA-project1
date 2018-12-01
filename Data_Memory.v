f7302dee1ac66322f08706c2baba6a41a0a36d67
陳代穎 <dying1020@gmail.com>
2018-12-01 16:46:56 +0800

    Fix Data_Memory bug

--- Data_Memory.v
+++ Data_Memory.v
@@ -21,17 +21,12 @@ wire    [31:0]  addr_i_shift;
 /*
 Notice: hard code due to only lw and sw instruction
 */
+// Addressing by Little Endian
 always@(MemWrite_i or MemRead_i or addr_i or data_i) begin
     if (MemWrite_i) // sw
-        memory[addr_i] = data_i[7:0];
-        memory[addr_i+1] = data_i[15:8];
-        memory[addr_i+2] = data_i[23:16];
-        memory[addr_i+3] = data_i[31:24];
+        memory[addr_i] = data_i;
     if (MemRead_i) // lw
-        data_o[7:0] = memory[addr_i];
-        data_o[15:8] = memory[addr_i+1];
-        data_o[23:16] = memory[addr_i+2];
-        data_o[31:24] = memory[addr_i+3];
+        data_o = memory[addr_i];
 end
 
 endmodule
