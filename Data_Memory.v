fe7f35d44f33d089a24fc402f0a61a74b7029c06
陳代穎 <dying1020@gmail.com>
2018-12-01 15:58:20 +0800

    Change Data_Memory to Byte address

--- Data_Memory.v
+++ Data_Memory.v
@@ -15,19 +15,23 @@ input   MemWrite_i;
 output reg  [31:0]  data_o;
 
 // Data memory
-/*
-Notice: hard code due to only lw and sw instruction
-*/
 reg     [7:0]  memory  [0:31]; // total size is 32 Bytes = 1 Byte * 32
 wire    [31:0]  addr_i_shift;
 
-assign addr_i_shift = addr_i >> 2;
-
+/*
+Notice: hard code due to only lw and sw instruction
+*/
 always@(MemWrite_i or MemRead_i or addr_i or data_i) begin
     if (MemWrite_i) // sw
-        memory[addr_i] = data_i;
+        memory[addr_i] = data_i[7:0];
+        memory[addr_i+1] = data_i[15:8];
+        memory[addr_i+2] = data_i[23:16];
+        memory[addr_i+3] = data_i[31:24];
     if (MemRead_i) // lw
-        data_o = memory[addr_i];
+        data_o[7:0] = memory[addr_i];
+        data_i[15:8] = memory[addr_i+1];
+        data_i[23:16] = memory[addr_i+2];
+        data_i[31:24] = memory[addr_i+3];
 end
 
 endmodule
