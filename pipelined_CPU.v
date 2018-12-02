ee8dabcdaea782ee8134855000d8a6cf4b451e06
b03201003 <b03201003@ntu.edu.tw>
2018-12-02 19:31:32 +0800

    stall,hazard,forwarding not yet

--- pipelined_CPU.v
+++ pipelined_CPU.v
@@ -80,28 +80,30 @@ IF_ID IF_ID_Reg(
 );
 
 
+//project1 new (Lin)
+wire	[31:0]	WriteBack_data;
 Registers Registers(
     .clk_i      (clk_i),
     .RS1addr_i   (inst[19:15]),
     .RS2addr_i   (inst[24:20]),
-    .RDaddr_i   (inst[11:7]),
-    .RDdata_i   (MUX_RegSrc.data_o),//project1 new
+    .RDaddr_i   (MEM_WB_RegisterRd),//inst[11:7]
+    .RDdata_i   (WriteBack_data),//from the MUX_RegSrc
     .RegWrite_i (Control.RegWrite_o),
     .RS1data_o   (ALU.data1_i),
     .RS2data_o   (MUX_ALUSrc.data1_i)
 );
 
 
-
+//project1 new (Lin)
 Sign_Extend Sign_Extend(
     .data_i     (inst[31:20]),
-    .data_o     ()
+    .data_o     (ID_EX_Reg.immi_sign_extended_in)
 );
 
 //project1 new (Lin)
 ID_EX ID_EX_Reg(
 	.ID_Flush_lwstall	(), //stall control for load instruction
-	.ID_Flush_Branch	(), //hazard unit
+	.ID_Flush_Branch	(), //hazard unit, control hazard
 	.RegWrite_in		(Control.RegWrite_o), 
 	.MemtoReg_in		(Control.MemtoReg_o), 
 	.RegWrite_out		(EX_MEM_Reg.RegWrite_in), 
@@ -110,32 +112,32 @@ ID_EX ID_EX_Reg(
 	.MemRead_in			(Control.MemRead_o), 
 	.MemWrite_in		(Control.MemWrite_o), 
 	.Jump_in			(), //from Control, needed?
-	.Branch_out			(),  //to whom?
+	.Branch_out			(EX_MEM_Reg.Branch_in), 
 	.MemRead_out		(EX_MEM_Reg.MemRead_in), 
 	.MemWrite_out		(EX_MEM_Reg.MemWrite_in), 
-	.Jump_out			(), 
+	.Jump_out			(EX_MEM_Reg.Jump_in), //for what?
 	.RegDst_in			(), //from Control, needed?
 	.ALUSrc_in			(Control.ALUSrc_o), 
-	.RegDst_out			(), 
-	.ALUSrc_out			(MUX_ALUSrc.select_i), 
+	.RegDst_out			(), //to EX_RegisterRd MUX(what is EX_RegisterRd MUX?)
+	.ALUSrc_out			(ALU_input2.ALUSrc), 
 	.ALUOp_in			(Control.ALUOp_o),
 	.ALUOp_out			(ALU_Control.ALUOp_i), 
-	.jump_addr_in		(),
-	.jump_addr_out		(),
+	.jump_addr_in		(), //how to calculate this in RISC-V?
+	.jump_addr_out		(EX_MEM_Reg.jump_addr_in), //(what different to Jump_out?)
 	.PC_plus4_in		(IF_ID_Reg.PC_plus4_out), 
 	.PC_plus4_out		(), //to the adder of the branch address
 	.reg_read_data_1_in	(), //from Mux_N_bit(what that?)
 	.reg_read_data_2_in	(), //from Mux_N_bit(what that?) 
 	.immi_sign_extended_in(Sign_Extend.data_o), 
-	.reg_read_data_1_out(), //to ALU_data1
-	.reg_read_data_2_out(), //to ALU_data2
-	.immi_sign_extended_out(MUX_ALUSrc.data2_i), 
-	.IF_ID_RegisterRs_in(), //inst[a:b]
+	.reg_read_data_1_out(ALU_input1.data1_i), //to ALU_input1
+	.reg_read_data_2_out(ALU_input2.data1_i), //to ALU_input2
+	.immi_sign_extended_out(ALU_input2.data4_i), 
+	.IF_ID_RegisterRs_in(), //inst[a:b], what is a,b in RISC-V
 	.IF_ID_RegisterRt_in(), //inst[a:b]
 	.IF_ID_RegisterRd_in(), //inst[a:b]
 	.IF_ID_RegisterRs_out(), //to forwarding unit
-	.IF_ID_RegisterRt_out(), //to forwarding unit 
-	.IF_ID_RegisterRd_out(), 
+	.IF_ID_RegisterRt_out(), //to forwarding unit, also to EX_RegisterRd MUX(why)
+	.IF_ID_RegisterRd_out(), //to EX_RegisterRd MUX
 	.IF_ID_funct_in		(),  //inst[a:b]
 	.IF_ID_funct_out	(),  //to ALU_Control(needed?)
 	.clk				(clk), 
@@ -143,23 +145,29 @@ ID_EX ID_EX_Reg(
 );
 
 //project1 new (Lin)
-MUX32_3to1 ALU_data1(
+wire 	[31:0]	EX_MEM_ALU_result;
+MUX32_3to1 ALU_input1(//input1 from Registers, input2 from WB MUX, input3 from ALU result(not implemented yet)
+    .data1_i    (ID_EX_Reg.reg_read_data_1_out),
+    .data2_i    (WriteBack_data),	
+    .data3_i    (EX_MEM_ALU_result),	
+	.select_i	(), //from forwarding unit
+	.data_o		(ALU.data1_i)
 );
-MUX32_3to1 ALU_data2(
+MUX32_4to1 ALU_input2(//input1 from Registers, input2 from WB MUX, input3 from ALU result, input4 from ID/EX imm(not implemented yet)
+    .data1_i    (ID_EX_Reg.reg_read_data_2_out),
+    .data2_i    (WriteBack_data),	
+    .data3_i    (EX_MEM_ALU_result),	
+	.select_i	(), //from forwarding unit(decide from first three input)
+	.data4_i    (ID_EX_Reg.immi_sign_extended_out),	//imm
+	.ALUSrc		(ID_EX_Reg.ALUSrc_out), //(decide to use imm or first three )
+	.data_o		(ALU.data2_i)
 );
 
-//project1 new (Lin)
-MUX32 MUX_ALUSrc(
-    .data1_i    (Registers.RS2data_o),
-    .data2_i    (ID_EX_Reg.immi_sign_extended_out),
-    .select_i   (ID_EX_Reg.ALUSrc_out),
-    .data_o     (ALU.data2_i)
-);
 
 //project1 new (Lin)
 ALU ALU(
-    .data1_i    (Registers.RS1data_o),
-    .data2_i    (MUX_ALUSrc.data_o),
+    .data1_i    (ALU_input1.data_o),
+    .data2_i    (ALU_input2.data_o),
     .ALUCtrl_i  (ALU_Control.ALUCtrl_o),
     .data_o     (EX_MEM_Reg.ALU_result_in),
     .Zero_o     (zero)
@@ -174,67 +182,70 @@ ALU_Control ALU_Control(
 );
 
 //project1 new (Lin)
+wire [4:0] EX_MEM_RegisterRd; //to forwarding unit
 EX_MEM EX_MEM_Reg(
-	.EX_Flush		(), 
-	.RegWrite_in	(), 
-	.MemtoReg_in	(), 
-	.RegWrite_out	(), 
-	.MemtoReg_out	(), 
-	.Branch_in		(), 
-	.MemRead_in		(), 
-	.MemWrite_in	(),
-	.Jump_in		(), 
-	.Branch_out		(),
-	.MemRead_out	(), 
-	.MemWrite_out	(),
-	.Jump_out		(), 
-	.jump_addr_in	(), 
+	.EX_Flush		(), //hazard unit, control hazard
+	.RegWrite_in	(ID_EX_Reg.RegWrite_out), 
+	.MemtoReg_in	(ID_EX_Reg.MemtoReg_out), 
+	.RegWrite_out	(MEM_WB_Reg.RegWrite_in), 
+	.MemtoReg_out	(MEM_WB_Reg.MemtoReg_in), 
+	.Branch_in		(ID_EX_Reg.Branch_out), 
+	.MemRead_in		(ID_EX_Reg.MemRead_out), 
+	.MemWrite_in	(ID_EX_Reg.MemWrite_out),
+	.Jump_in		(ID_EX_Reg.Jump_out), 
+	.Branch_out		(), // to the branch and-gate(result to the jump_OR_branch unit)
+	.MemRead_out	(Data_Memory.MemRead_i), 
+	.MemWrite_out	(Data_Memory.MemWrite_i),
+	.Jump_out		(), //to jump_OR_branch unit(what that?)
+	.jump_addr_in	(ID_EX_Reg.jump_addr_out), 
 	.branch_addr_in	(),
-	.jump_addr_out	(),
-	.branch_addr_out(),
+	.jump_addr_out	(), //to jump_OR_branch unit
+	.branch_addr_out(), //to jump_OR_branch unit
 	.ALU_zero_in	(zero), 
-	.ALU_zero_out	(),
+	.ALU_zero_out	(), // to the branch and-gate
 	.ALU_result_in	(ALU.data_o),
-	.reg_read_data_2_in(),
-	.ALU_result_out	(Data_Memory.addr_i),
-	.reg_read_data_2_out(), 
-	.ID_EX_RegisterRd_in(), 
-	.EX_MEM_RegisterRd_out(), 
-	.clk			(), 
-	.reset			()
+	.reg_read_data_2_in(ALU_input2.data_o), //from ALU_input2(mux3to1_B)
+	.ALU_result_out	(EX_MEM_ALU_result),
+	.reg_read_data_2_out(Data_Memory.data1_i), 
+	.ID_EX_RegisterRd_in(), //from EX_RegisterRd MUX
+	.EX_MEM_RegisterRd_out(EX_MEM_RegisterRd), 
+	.clk			(clk), 
+	.reset			(reset)
 );
 
 //project1 new (Lin)
 Data_Memory Data_Memory(
-    .addr_i     (ALU.data_o),
-    .data_i     (Registers.RS2data_o),
-    .MemRead_i  (),
-    .MemWrite_i (),
-    .data_o     (MUX_RegSrc.data2_i)
+    .addr_i     (EX_MEM_ALU_result),
+    .data_i     (EX_MEM_Reg.reg_read_data_2_out),
+    .MemRead_i  (EX_MEM_Reg.MemRead_out),
+    .MemWrite_i (EX_MEM_Reg.MemWrite_out),
+    .data_o     (MEM_WB_Reg.D_MEM_read_data_in),
+	.clk		(clk)
 );
 
 //project1 new (Lin)
+wire 	[4:0]	MEM_WB_RegisterRd;//determine which register to write back
 MEM_WB MEM_WB_Reg(
-	.RegWrite_in		(), 
-	.MemtoReg_in		(), 
-	.RegWrite_out		(), 
-	.MemtoReg_out		(), 
-	.D_MEM_read_data_in	(), 
-	.D_MEM_read_addr_in	(),
-	.D_MEM_read_data_out(), 
-	.D_MEM_read_addr_out(), 
-	.EX_MEM_RegisterRd_in(), 
-	.MEM_WB_RegisterRd_out(), 
-	.clk				(), 
-	.reset				()
+	.RegWrite_in		(EX_MEM_Reg.RegWrite_out), 
+	.MemtoReg_in		(EX_MEM_Reg.MemtoReg_out), 
+	.RegWrite_out		(), //to forwarding unit
+	.MemtoReg_out		(MUX_RegSrc.select_i), 
+	.D_MEM_read_data_in	(Data_Memory.data_o), 
+	.D_MEM_read_addr_in	(EX_MEM_ALU_result),
+	.D_MEM_read_data_out(MUX_RegSrc.data1_i), 
+	.D_MEM_read_addr_out(MUX_RegSrc.data2_i), 
+	.EX_MEM_RegisterRd_in(EX_MEM_RegisterRd), 
+	.MEM_WB_RegisterRd_out(MEM_WB_RegisterRd), 
+	.clk				(clk), 
+	.reset				(reset)
 );
 
-
-MUX32 MUX_RegSrc(
-    .data1_i    (ALU.data_o),
-    .data2_i    (Data_Memory.data_o),
-    .select_i   (Control.MemtoReg_o),
-    .data_o     (Registers.RDdata_i)
+//project1 new (Lin)
+MUX32 MUX_RegSrc(//WB MUX
+    .data1_i    (MEM_WB_Reg.D_MEM_read_data_out),
+    .data2_i    (MEM_WB_Reg.D_MEM_read_addr_out),
+    .select_i   (MEM_WB_Reg.MemtoReg_out),
+    .data_o     (WriteBack_data)
 );
 
 
