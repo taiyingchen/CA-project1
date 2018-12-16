7f83ca1e10ea18226ec26d5537ed991846ad054e
peiwen <judyazby520@gmail.com>
2018-12-16 18:32:00 +0800

    rs rt to rs1 rs2

--- ID_EX.v
+++ ID_EX.v
@@ -2,8 +2,8 @@ module ID_EX (ID_Flush_lwstall,  RegWrite_in, MemtoReg_in, RegWrite_out,
 	MemtoReg_out, MemRead_in, MemWrite_in, MemRead_out, MemWrite_out, ALUSrc_in, 
 	ALUSrc_out, ALUOp_in, ALUOp_out, reg_read_data_1_in, reg_read_data_2_in, 
 	immi_sign_extended_in, reg_read_data_1_out, reg_read_data_2_out, immi_sign_extended_out, 
-	IF_ID_RegisterRs_in, IF_ID_RegisterRt_in, IF_ID_RegisterRd_in, IF_ID_RegisterRs_out, 
-	IF_ID_RegisterRt_out, IF_ID_RegisterRd_out, clk, reset);
+	IF_ID_RegisterRs1_in, IF_ID_RegisterRs2_in, IF_ID_RegisterRd_in, IF_ID_RegisterRs1_out, 
+	IF_ID_RegisterRs2_out, IF_ID_RegisterRd_out, clk, /*reset*/);
 	// 1. hazard control signal (sync rising edge)
 	// if either ID_Flush_lwstall or ID_Flush_Branch equals 1,
 	// then clear all WB, MEM and EX control signal to 0 on rising edge
@@ -27,13 +27,13 @@ module ID_EX (ID_Flush_lwstall,  RegWrite_in, MemtoReg_in, RegWrite_out,
 	input [31:0] reg_read_data_1_in, reg_read_data_2_in, immi_sign_extended_in;
 	output [31:0] reg_read_data_1_out, reg_read_data_2_out, immi_sign_extended_out;
 	// 7. reg content
-	input [4:0] IF_ID_RegisterRs_in, IF_ID_RegisterRt_in, IF_ID_RegisterRd_in;
-	output [4:0] IF_ID_RegisterRs_out, IF_ID_RegisterRt_out, IF_ID_RegisterRd_out;
+	input [4:0] IF_ID_RegisterRs1_in, IF_ID_RegisterRs2_in, IF_ID_RegisterRd_in;
+	output [4:0] IF_ID_RegisterRs1_out, IF_ID_RegisterRs2_out, IF_ID_RegisterRd_out;
 	// input [5:0] IF_ID_funct_in;
 	// output [5:0] IF_ID_funct_out;
 	// general signal
 	// reset: async; set all register content to 0
-	input clk, reset;
+	input clk/*, reset*/;
 	
 	reg RegWrite_out, MemtoReg_out;
 	reg Branch_out, MemRead_out, MemWrite_out;
@@ -41,11 +41,12 @@ module ID_EX (ID_Flush_lwstall,  RegWrite_in, MemtoReg_in, RegWrite_out,
 	reg [1:0] ALUOp_out;
 	// reg [31:0] PC_out;
 	reg [31:0] reg_read_data_1_out, reg_read_data_2_out, immi_sign_extended_out;
-	reg [4:0] IF_ID_RegisterRs_out, IF_ID_RegisterRt_out, IF_ID_RegisterRd_out;
+	reg [4:0] IF_ID_RegisterRs1_out, IF_ID_RegisterRs2_out, IF_ID_RegisterRd_out;
 	reg [5:0] IF_ID_funct_out;
 	
-	always @(posedge clk or posedge reset)
+	always @(posedge clk/* or posedge reset*/)
 	begin
+		/*
 		if (reset == 1'b1)
 		begin
 			RegWrite_out = 1'b0;
@@ -60,12 +61,12 @@ module ID_EX (ID_Flush_lwstall,  RegWrite_in, MemtoReg_in, RegWrite_out,
 			reg_read_data_1_out = 32'b0;
 			reg_read_data_2_out = 32'b0;
 			immi_sign_extended_out = 32'b0;
-			IF_ID_RegisterRs_out = 5'b0;
-			IF_ID_RegisterRt_out = 5'b0;
+			IF_ID_RegisterRs1_out = 5'b0;
+			IF_ID_RegisterRs2_out = 5'b0;
 			IF_ID_RegisterRd_out = 5'b0;
 			// IF_ID_funct_out = 6'b0;			
 		end
-		else if (ID_Flush_lwstall == 1'b1)
+		else*/ if (ID_Flush_lwstall == 1'b1)
 		begin
 			RegWrite_out = 1'b0;
 			MemtoReg_out = 1'b0;
@@ -100,8 +101,8 @@ module ID_EX (ID_Flush_lwstall,  RegWrite_in, MemtoReg_in, RegWrite_out,
 			reg_read_data_1_out = reg_read_data_1_in;
 			reg_read_data_2_out = reg_read_data_2_in;
 			immi_sign_extended_out = immi_sign_extended_in;
-			IF_ID_RegisterRs_out = IF_ID_RegisterRs_in;
-			IF_ID_RegisterRt_out = IF_ID_RegisterRt_in;
+			IF_ID_RegisterRs1_out = IF_ID_RegisterRs1_in;
+			IF_ID_RegisterRs2_out = IF_ID_RegisterRs2_in;
 			IF_ID_RegisterRd_out = IF_ID_RegisterRd_in;
 			// IF_ID_funct_out = IF_ID_funct_in;
 		end	
